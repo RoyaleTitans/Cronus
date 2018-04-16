7b99b954a0e95255b7a2d184fa23d97d8b96dde2
iGio90 <giovanni.rocca.90@gmail.com>
2018-04-16 14:10:19 +0200

    kill dbg stuffs

--- src/com/royale/titans/cronus/Main.java
+++ src/com/royale/titans/cronus/Main.java
@@ -1,24 +1,9 @@
 package com.royale.titans.cronus;
 
-import com.royale.titans.cronus.lib.Buffer;
-import jdk.jfr.Unsigned;
-
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Buffer v = Buffer.allocate(10);
-        v.writeRrsInt(1503298800);
-        v.rewind();
-        System.out.println(v.readRrsInt().getValue());
-        System.out.println(Utils.b2h(v.array()));
-
-        v.clear();
-        v.writeRrsInt(257080);
-        v.rewind();
-        System.out.println(v.readRrsInt().getValue());
-
         Server server = new Server();
         server.connect();
 
